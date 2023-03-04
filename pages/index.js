fcdb1a06f60fdf1ced0f502884db7464ac1ab2ab
fire <46013177+frickinfire@users.noreply.github.com>
2023-03-03 20:20:33 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from 'https://ntvb.tmsimg.com/assets/assets/73227_v9_bb.jpg?w=270&h=360'; // Tell webpack this JS file uses this image
+import logo from './kenan.jpg'; // Tell webpack this JS file uses this image
 
 console.log(logo); // /logo.84287d09.png
 
 function Header() {
   // Import result is the URL of your image
-  return <img src={logo} alt="Logo" />;
+  return <img src={logo} alt="kenan" />;
 }
 
 export default Header;
