feb00f593e4224d36044dcb42a6556a845c093e0
fire <46013177+frickinfire@users.noreply.github.com>
2023-03-03 20:19:20 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from 'react';
+import logo from 'https://ntvb.tmsimg.com/assets/assets/73227_v9_bb.jpg?w=270&h=360'; // Tell webpack this JS file uses this image
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Kenan Thompson Fanpage</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+console.log(logo); // /logo.84287d09.png
 
-      <main>
-        <image src="https://ntvb.tmsimg.com/assets/assets/73227_v9_bb.jpg?w=270&h=360"/>
-        <p className="description">
-         Get Kenaned, Dennis
-        </p>
-      </main>
-    </div>
-  )
+function Header() {
+  // Import result is the URL of your image
+  return <img src={logo} alt="Logo" />;
 }
+
+export default Header;
