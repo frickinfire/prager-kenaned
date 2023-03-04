3317ccc1baaa07f8e99f89a4ea3e65981e6a82a6
fire <46013177+frickinfire@users.noreply.github.com>
2023-03-03 20:14:56 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const MyImage = (props) => {
   return (
     <Image
       loader={myLoader}
-      src=https://ntvb.tmsimg.com/assets/assets/73227_v9_bb.jpg?w=270&h=360"
+      src="https://ntvb.tmsimg.com/assets/assets/73227_v9_bb.jpg?w=270&h=360"
       alt="kenan"
       width={500}
       height={500}
