472bb44e7afea6106c44b7457f8bb38c1959ad5c
fire <46013177+frickinfire@users.noreply.github.com>
2023-08-07 10:43:41 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Header() {
   // Import result is the URL of your image
-  return <iframe src="./assets/index.html" frameborder="0"></iframe>;
+  return <iframe src="https://frickinfire.com/transrights" frameborder="0"></iframe>;
 }
 
 export default Header;
