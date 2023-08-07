08722f9360fc1ace12b004937a6ae35a5269af4b
fire <46013177+frickinfire@users.noreply.github.com>
2023-08-07 10:42:00 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Header() {
   // Import result is the URL of your image
-  return <img src="https://ntvb.tmsimg.com/assets/assets/73227_v9_bb.jpg?w=270&h=360" alt="kenan" />;
+  return <iframe src="./assets/index.html" frameborder="0"></iframe>;
 }
 
 export default Header;
